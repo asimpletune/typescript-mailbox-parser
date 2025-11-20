a6b0c3a75c174b5d6bd502b716f274728f8cddc7
Spencer Scorcelletti <2670813+asimpletune@users.noreply.github.com>
2025-11-20 13:45:55 +0100

    Update with better support for quoted strings in the local portion of a mailbox address (#1)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,14 @@
 
 ## Next
 
+## 0.0.2 (2025-11-20)
+
+- Improve support for quoted strings in the local portion of an email
+- Export the `mailbox` function by default
+- Return a discriminated union instead of a success object or array of error strings
+- Automate generating the example code used in the README
+- Add explicit license
+
 ## 0.0.1 (2025-09-23)
 
 - Document how it's used and why there won't be many updates
---
+++ examples.ts
@@ -0,0 +1,31 @@
+import mailbox from './dist'
+import vm from 'node:vm'
+
+const sandbox = { mailbox, console }
+vm.createContext(sandbox)
+
+const examples = [
+  `mailbox('<bob@example.com>')`,
+  `mailbox('Bob Hope <bob@example.com>')`,
+  `mailbox('"Bob Hope" <bob@example.com>')`,
+  `mailbox('Bruce "The Boss" Springsteen <bruce@example.com>')`,
+  `mailbox('bob@example')`,
+  `mailbox('BOB@example')`,
+  `mailbox('bob@EXAMPLE')`,
+  `mailbox('a.b.c@d.e.f.g')`,
+  `mailbox('"site.local.test:1111"@example.com')`,
+  `mailbox('"hello, world"@example.com')`,
+  `mailbox('foo bar baz')`,
+]
+
+for (const e of examples) {
+  const result = vm.runInContext(e, sandbox)
+  // Print the result as a commented block
+  const commented = JSON.stringify(result, null, 2)
+    .split('\n')
+    .map((line) => `// ${line}`)
+    .join('\n')
+
+  console.log(commented)
+  console.log(`console.log(${e})\n`)
+}
